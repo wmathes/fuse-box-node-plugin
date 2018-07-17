b204ecd8d065b19d14409922d9e39a2d0363c3cd
Wolf Mathes <wmathes@users.noreply.github.com>
2018-07-17 16:18:56 +0700

    fix(NodePlugin): respect root directory

--- src/NodePlugin.ts
+++ src/NodePlugin.ts
@@ -5,6 +5,7 @@ import * as glob from "glob";
 import * as path from "path";
 import { utils } from "realm-utils";
 import { string2RegExp, hashString, joinFuseBoxPath } from "fuse-box/Utils";
+import {ensureDirSync} from "fs-extra";
 
 export interface NodePluginOptions {
     file: string;
@@ -18,16 +19,13 @@ export class NodePluginClass implements Plugin {
 
     private dest: string;
 
-    private resolve: string;
-
     constructor(public options: NodePluginOptions) {
         if (!options || !options.file) {
             throw new Error("invalid options provided");
         }
 
-        const identifier = options.identifier || path.basename(options.file);
-        this.dest = `node/${identifier}`;
-        this.resolve = `/${this.dest}/`;
+        const identifier = options.identifier || path.basename(options.file, ".node");
+        this.dest = `modules/${identifier}`;
         this.test = string2RegExp(options.file);
     }
 
@@ -37,14 +35,15 @@ export class NodePluginClass implements Plugin {
 
     protected copyDependencies(context: WorkFlowContext, patterns: string[], sourcePathAbs: string, targetDirAbs: string): void {
         patterns.forEach((pattern) => {
-            glob.sync(path.join(sourcePathAbs, pattern), { absolute: true })
+            glob.sync(path.join(sourcePathAbs, pattern), { nodir: true, absolute: true })
                 .forEach((fileAbs: string) => {
                     const relativePath = path.relative(sourcePathAbs, fileAbs);
                     if (relativePath.indexOf("../") > -1) {
                         throw new Error(`relativePath "${relativePath}" outside of bounds. Use root-params to correct.`);
                     }
                     const targetPathAbs = path.resolve(context.output.dir, this.dest, relativePath);
-                    console.log(`COPY ${fileAbs} => ${targetPathAbs}\n`);
+                    ensureDirSync(path.dirname(targetPathAbs));
+                    //console.log(`COPY ${fileAbs} => ${targetPathAbs}\n`);
                     copyFileSync(fileAbs, targetPathAbs);
                 });
         })
@@ -71,11 +70,12 @@ export class NodePluginClass implements Plugin {
 
         // filename
         let userFile = (!context.hash ? hashString(file.info.fuseBoxPath) + "-" : "") + path.basename(file.info.fuseBoxPath);
+        let userDest = path.join(this.dest, path.dirname(file.info.fuseBoxPath));
 
         // output file path
-        let userPath = path.join(this.dest, userFile);
+        let userPath = path.join(userDest, userFile);
 
-        file.alternativeContent = (`module.exports = require(".${joinFuseBoxPath(this.resolve, userFile)}");`);
+        file.alternativeContent = (`module.exports = require("./${joinFuseBoxPath(userDest, userFile)}");`);
 
         return new Promise((resolve: any, reject: any) => {
             readFile(file.absPath, (err, data) => {
@@ -84,7 +84,7 @@ export class NodePluginClass implements Plugin {
                 }
                 return context.output.writeToOutputFolder(userPath, data, true).then(result => {
                     if (result.hash) {
-                        file.alternativeContent = (`module.exports = require(".${joinFuseBoxPath(this.resolve, result.filename)}");`);
+                        file.alternativeContent = (`module.exports = require("./${joinFuseBoxPath(userDest, result.filename)}");`);
                     }
 
                     if (utils.isArray(options.relativeDependencies)) {
